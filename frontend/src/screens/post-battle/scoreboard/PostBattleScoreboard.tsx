18747b22b0136abc3831b13aa108a7bbac32a88d
makstashkevich <makstashkevich@gmail.com>
2023-02-17 20:53:00 +0300

    Add scoreboard table

--- frontend/src/screens/post-battle/scoreboard/PostBattleScoreboard.tsx
+++ frontend/src/screens/post-battle/scoreboard/PostBattleScoreboard.tsx
@@ -1,35 +1,69 @@
 import React from 'react';
 import style from "./PostBattleScoreboard.module.scss";
+import {convertNumber, getRandomInt} from "../../../utils/math";
 
-const PostBattleScoreboard = () => {
+interface ScoreboardLineProps {
+    id: number;
+    username: string;
+    alive: boolean;
+    score: number;
+}
+
+const ScoreboardLine = ({id, username, alive, score}: ScoreboardLineProps) => {
     return (
-        <div className={style.scoreboard}>
-            <table>
+        <tr>
+            <td>{id}</td>
+            <td>{username}</td>
+            <td>{alive ? 'Alive' : 'Dead'}</td>
+            <td>{convertNumber(score) + ' pts'}</td>
+        </tr>
+    )
+}
+
+const ScoreboardTeamTable = () => {
+    const teamUsers: ScoreboardLineProps[] = [];
+    for (let i = 0; i < 50; i++) {
+        teamUsers.push({
+            id: i + 1,
+            username: 'Nickname',
+            alive: Boolean(Math.random() < 0.5),
+            score: getRandomInt(0, 12345678)
+        })
+    }
+    let totalScore = 0;
+    teamUsers.map(v => totalScore += v.score)
+    return (
+        <div>
+            <table className={style.scoreTable}>
+                <thead>
                 <tr>
                     <th>#</th>
                     <th>Player</th>
                     <th>Status</th>
                     <th>Score</th>
                 </tr>
+                </thead>
+                <tbody>
+                {teamUsers.map((v) =>
+                    <ScoreboardLine id={v.id} username={v.username} alive={v.alive} score={v.score}/>
+                )}
+                </tbody>
+                <tfoot>
                 <tr>
-                    <td>1</td>
-                    <td>Nickname</td>
-                    <td>Dead</td>
-                    <td>1,234,567</td>
+                    <th>Total</th>
+                    <th>{convertNumber(totalScore)}</th>
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Nickname</td>
-                    <td>Dead</td>
-                    <td>1,234,567</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Nickname</td>
-                    <td>Dead</td>
-                    <td>1,234,567</td>
-                </tr>
+                </tfoot>
             </table>
+        </div>
+    )
+}
+
+const PostBattleScoreboard = () => {
+    return (
+        <div className={style.scoreboard}>
+            <ScoreboardTeamTable/>
+            <ScoreboardTeamTable/>
         </div>
     );
 };
