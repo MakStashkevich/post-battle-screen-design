c9401522f6a616902161de2b4d36da6758437e2e
makstashkevich <makstashkevich@gmail.com>
2023-02-19 10:01:36 +0300

    Bit fix api route

--- api/src/battle/routes.py
+++ api/src/battle/routes.py
@@ -6,6 +6,6 @@ from .schemas import PostBattleTeamSchema
 router = APIRouter(prefix="/battle")
 
 
-@router.get("/endgame", response_model=list[PostBattleTeamSchema])
+@router.get("/endgame/", response_model=list[PostBattleTeamSchema])
 def get_post_battle_results():
     return BattleRequest.get_teams_post_battle()
